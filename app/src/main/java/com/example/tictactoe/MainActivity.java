fc47e8f217326ab0eb00b3a91619ad9da99bbff8
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-22 01:25:58 +0530

    define winningPlayer().

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tictactoe;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -246,6 +248,53 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void winningGame(){
+        //we have 16 cases of winning 8 for cross and 8 for zero.
+        if(((b1==1) && (b2==1) && (b3==1)) ||((b1==1) && (b4==1) &&(b7==1)) || ((b2==1) && (b5==1)&&(b8==1)) ||((b4==1)&&(b5==1)&&(b6==1))||
+                ((b3==1)&&(b6==1)&&(b9==1)) ||((b7==1)&&(b8==1)&&(b9==1))){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Player X Wins").setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    button1.setImageDrawable(null);
+                    button2.setImageDrawable(null);
+                    button3.setImageDrawable(null);
+                    button4.setImageDrawable(null);
+                    button5.setImageDrawable(null);
+                    button6.setImageDrawable(null);
+                    button7.setImageDrawable(null);
+                    button8.setImageDrawable(null);
+                    button9.setImageDrawable(null);
+                    resetValue();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            xCount++;
+            scoreX.setText("Player X : "+xCount);
 
+        }
+        else if(((b1==0) && (b2==0) && (b3==0)) ||((b1==0) && (b4==0) &&(b7==0)) || ((b2==0) && (b5==0)&&(b8==0)) ||((b4==0)&&(b5==0)&&(b6==0))||
+                ((b3==0)&&(b6==0)&&(b9==0)) ||((b7==0)&&(b8==0)&&(b9==0))){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Player O Wins").setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    button1.setImageDrawable(null);
+                    button2.setImageDrawable(null);
+                    button3.setImageDrawable(null);
+                    button4.setImageDrawable(null);
+                    button5.setImageDrawable(null);
+                    button6.setImageDrawable(null);
+                    button7.setImageDrawable(null);
+                    button8.setImageDrawable(null);
+                    button9.setImageDrawable(null);
+                    resetValue();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+            oCount++;
+            scoreX.setText("Player X : "+oCount);
+        }
     }
 }
