f6ab946cfd35b91b9fbafaf22e846f0f28fd6f49
github <pragya.sharma2_cs19@gla.ac.in>
2021-06-22 01:38:13 +0530

    rearranging the code

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,8 +1,5 @@
 package com.example.tictactoe;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +7,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
     ImageView button1, button2, button3, button4, button5, button6, button7, button8, button9;
     Button reset;
     private String startGame = "X";
     private int b1 = 5, b2 = 5, b3 = 5, b4 = 5, b5 = 5, b6 = 5, b7 = 5, b8 = 5, b9 = 5, xCount = 0, oCount = 0, i = 0;
     private TextView scoreX, scoreO;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +30,8 @@ public class MainActivity extends AppCompatActivity {
         button7 = findViewById(R.id.btnImage7);
         button8 = findViewById(R.id.btnImage8);
         button9 = findViewById(R.id.btnImage9);
-        scoreX=findViewById(R.id.scoreX);
-        scoreO=findViewById(R.id.score0);
+        scoreX = findViewById(R.id.scoreX);
+        scoreO = findViewById(R.id.score0);
         reset = findViewById(R.id.reset);
         onClickReset();
         onClickButton1();
@@ -44,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         onClickButton9();
 
     }
-    private void onClickReset(){
+
+    private void onClickReset() {
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,47 +60,56 @@ public class MainActivity extends AppCompatActivity {
                 button8.setImageDrawable(null);
                 button9.setImageDrawable(null);
                 resetValue();
-                xCount=0;
-                oCount=0;
+                xCount = 0;
+                oCount = 0;
                 scoreX.setText("Score X : 0 ");
                 scoreO.setText("Score 0 : 0 ");
             }
         });
     }
-    private void resetValue(){
-        b1 = 5; b2 = 5; b3 = 5; b4 = 5; b5 = 5; b6 = 5; b7 = 5; b8 = 5; b9 = 5;
+
+    private void resetValue() {
+        b1 = 5;
+        b2 = 5;
+        b3 = 5;
+        b4 = 5;
+        b5 = 5;
+        b6 = 5;
+        b7 = 5;
+        b8 = 5;
+        b9 = 5;
     }
-    private void onClickButton1(){
+
+    private void onClickButton1() {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               if(startGame.equals("X")){
-                   button1.setImageResource(R.drawable.cross);
-                   b1=1;
-                   i++;
-               }
-               else{
-                   button1.setImageResource(R.drawable.circle);
-                   b1=0;
-                   i++;
-               }
-               choosePlayer();
-               winningGame();
+                if (startGame.equals("X")) {
+                    button1.setImageResource(R.drawable.cross);
+                    b1 = 1;
+                    i++;
+                } else {
+                    button1.setImageResource(R.drawable.circle);
+                    b1 = 0;
+                    i++;
+                }
+                choosePlayer();
+                winningGame();
             }
         });
     }
-    private void onClickButton2(){
+
+    private void onClickButton2() {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button2.setImageResource(R.drawable.cross);
-                    b2=1;
+                    b2 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button2.setImageResource(R.drawable.circle);
-                    b2=0;
+                    b2 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -106,18 +117,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void onClickButton3(){
+
+    private void onClickButton3() {
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button3.setImageResource(R.drawable.cross);
-                    b3=1;
+                    b3 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button3.setImageResource(R.drawable.circle);
-                    b3=0;
+                    b3 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -125,18 +136,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void onClickButton4(){
+
+    private void onClickButton4() {
         button4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button4.setImageResource(R.drawable.cross);
-                    b4=1;
+                    b4 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button4.setImageResource(R.drawable.circle);
-                    b4=0;
+                    b4 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -144,18 +155,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void onClickButton5(){
+
+    private void onClickButton5() {
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button5.setImageResource(R.drawable.cross);
-                    b5=1;
+                    b5 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button5.setImageResource(R.drawable.circle);
-                    b5=0;
+                    b5 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -163,18 +174,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void onClickButton6(){
+
+    private void onClickButton6() {
         button6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button6.setImageResource(R.drawable.cross);
-                    b6=1;
+                    b6 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button6.setImageResource(R.drawable.circle);
-                    b6=0;
+                    b6 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -182,18 +193,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void onClickButton7(){
+
+    private void onClickButton7() {
         button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button7.setImageResource(R.drawable.cross);
-                    b7=1;
+                    b7 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button7.setImageResource(R.drawable.circle);
-                    b7=0;
+                    b7 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -201,18 +212,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void onClickButton8(){
+
+    private void onClickButton8() {
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button8.setImageResource(R.drawable.cross);
-                    b8=1;
+                    b8 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button8.setImageResource(R.drawable.circle);
-                    b8=0;
+                    b8 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -220,18 +231,18 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void onClickButton9(){
+
+    private void onClickButton9() {
         button9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(startGame.equals("X")){
+                if (startGame.equals("X")) {
                     button9.setImageResource(R.drawable.cross);
-                    b9=1;
+                    b9 = 1;
                     i++;
-                }
-                else{
+                } else {
                     button9.setImageResource(R.drawable.circle);
-                    b9=0;
+                    b9 = 0;
                     i++;
                 }
                 choosePlayer();
@@ -239,18 +250,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    private void choosePlayer(){
-        if(startGame.equals("X")){
-            startGame="O";
-        }
-        else{
-            startGame="X";
+
+    private void choosePlayer() {
+        if (startGame.equals("X")) {
+            startGame = "O";
+        } else {
+            startGame = "X";
         }
     }
-    private void winningGame(){
+
+    private void winningGame() {
         //we have 16 cases of winning 8 for cross and 8 for zero.
-        if(((b1==1) && (b2==1) && (b3==1)) ||((b1==1) && (b4==1) &&(b7==1)) || ((b2==1) && (b5==1)&&(b8==1)) ||((b4==1)&&(b5==1)&&(b6==1))||
-                ((b3==1)&&(b6==1)&&(b9==1)) ||((b7==1)&&(b8==1)&&(b9==1))){
+        if (((b1 == 1) && (b2 == 1) && (b3 == 1)) || ((b1 == 1) && (b4 == 1) && (b7 == 1)) || ((b2 == 1) && (b5 == 1) && (b8 == 1)) || ((b4 == 1) && (b5 == 1) && (b6 == 1)) ||
+                ((b3 == 1) && (b6 == 1) && (b9 == 1)) || ((b7 == 1) && (b8 == 1) && (b9 == 1))) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Player X Wins").setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
@@ -270,11 +282,10 @@ public class MainActivity extends AppCompatActivity {
             AlertDialog alertDialog = builder.create();
             alertDialog.show();
             xCount++;
-            scoreX.setText("Score X : "+xCount);
+            scoreX.setText("Score X : " + xCount);
 
-        }
-        else if(((b1==0) && (b2==0) && (b3==0)) ||((b1==0) && (b4==0) &&(b7==0)) || ((b2==0) && (b5==0)&&(b8==0)) ||((b4==0)&&(b5==0)&&(b6==0))||
-                ((b3==0)&&(b6==0)&&(b9==0)) ||((b7==0)&&(b8==0)&&(b9==0))) {
+        } else if (((b1 == 0) && (b2 == 0) && (b3 == 0)) || ((b1 == 0) && (b4 == 0) && (b7 == 0)) || ((b2 == 0) && (b5 == 0) && (b8 == 0)) || ((b4 == 0) && (b5 == 0) && (b6 == 0)) ||
+                ((b3 == 0) && (b6 == 0) && (b9 == 0)) || ((b7 == 0) && (b8 == 0) && (b9 == 0))) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Player O Wins").setCancelable(false).setPositiveButton("OK", new DialogInterface.OnClickListener() {
                 @Override
@@ -295,9 +306,8 @@ public class MainActivity extends AppCompatActivity {
             alertDialog.show();
             oCount++;
             scoreO.setText("Score 0 : " + oCount);
-        }
-        else if(b1!=5 && b2!=5 && b3!=5 && b4!=5 && b5!=5 && b6!=5 &&
-                b7!=5 && b8!=5 && b9!=5 ){
+        } else if (b1 != 5 && b2 != 5 && b3 != 5 && b4 != 5 && b5 != 5 && b6 != 5 &&
+                b7 != 5 && b8 != 5 && b9 != 5) {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Are you both noob.Try to win again!!").setCancelable(false).setPositiveButton("Try Again", new DialogInterface.OnClickListener() {
                 @Override
